f7a2de2ae8b675d444eab4fa1b79435880936847
Carlos Martinez <carlosimportant@gmail.com>
2025-06-20 16:59:37 +0000

    bug fix

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function setup() //called once the program starts
 {
     createCanvas(400,400);
     backgroundColor(200);
+    square(250, cH,100)
 }
 
 function draw() //runs the loop to update the display
